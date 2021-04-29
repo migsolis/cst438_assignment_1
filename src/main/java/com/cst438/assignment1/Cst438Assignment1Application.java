db83dbbff4716c9c3667c1aaa8c46111f401897b
msolis <msolis@192.168.2.8>
2021-04-29 13:05:09 -0700

    Initial commit...

---
+++ src/main/java/com/cst438/assignment1/Cst438Assignment1Application.java
@@ -0,0 +1,13 @@
+package com.cst438.assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438Assignment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cst438/assignment1/Cst438Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cst438.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
